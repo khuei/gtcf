b6c88badc041ebc6a1005e35b6b24088404e979f
Khue Nguyen <Z5483Y@gmail.com>
2020-05-30 21:13:57 -0400

    add automate script to speed up setting up base system process

---
+++ setup-base.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+REPO=$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)
+
+# setting the date and time
+ntpd -q -g
+
+cd /mnt/gentoo
+
+# unpack stage3
+tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
+
+# add basic config files
+cp $REPO/etc/portage/make.conf /mnt/gentoo/etc/portage/make.conf
+cp -r $REPO/etc/portage/env /mnt/gentoo/etc/portage/env
+
+mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
+
+mkdir --parents /mnt/gentoo/etc/portage/repos.conf
+cp $REPO/etc/portage/repos.conf/overlay.conf /mnt/gentoo/etc/portage/repos.conf
+
+cp $REPO/etc/resolve.conf* /mnt/gentoo/etc
+
+# mount necessary filesystems
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+chmod 1777 /dev/shm
